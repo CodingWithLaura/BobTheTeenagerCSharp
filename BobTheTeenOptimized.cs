4ef3e8cc0ec5c8c8d7b8243e8c07443b5eea2d1c
Laura Ganss <laura.ganss@lise.de>
2021-03-23 18:48:44 +0100

    Reduce side-effects by using switch expressions

---
+++ BobTheTeenOptimized.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Bob
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string userSentence = Console.ReadLine();
+            Console.WriteLine(Response(userSentence));
+        }
+
+        public static string Response(string sentence) =>
+            sentence switch
+            {
+                var s when s.EndsWith("?") && s.Any(char.IsUpper) => "Calm down, I know what I'm doing!",
+                var s when s.EndsWith("?") => "Sure.",
+                var s when s.Any(char.IsUpper) => "Whoa, chill out!",
+                var s when s.Equals("") => "Fine. Be that way!",
+                _ => "Whatever."
+            };
+    }
+}
